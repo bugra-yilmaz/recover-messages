d4a203a4215d968821bb274f3d0f2cb4acdfcf95
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2020-10-04 16:38:21 +0300

    Return training history after training RNN model

--- rnn_model.py
+++ rnn_model.py
@@ -1,7 +1,7 @@
 import os
 from keras.layers import Dense
 from keras.layers import Dropout
-from keras.layers import SimpleRNN
+from keras.layers import GRU
 from keras.models import Sequential
 from keras.models import load_model
 from data_preprocessing import get_rnn_data
@@ -9,7 +9,7 @@ from keras.layers.embeddings import Embedding
 
 
 # Builds a simple RNN model with the given parameters
-def build_rnn_model(embedding_size=64, rnn_units=32, optimizer='adam', rnn_type=SimpleRNN,
+def build_rnn_model(embedding_size=64, rnn_units=32, optimizer='adam', rnn_type=GRU,
                     model_depth=1, dropout_rate=0):
     rnn_model = Sequential()
     rnn_model.add(Embedding(input_dim=33, output_dim=embedding_size, input_length=8))
@@ -24,15 +24,18 @@ def build_rnn_model(embedding_size=64, rnn_units=32, optimizer='adam', rnn_type=
     return rnn_model
 
 
-# Trains the RNN model with given parameters and saves it into a .h5 file
+# Trains the RNN model with given parameters and saves it into a .h5 file, returns training history
 def train_rnn_model(model_params, epochs, batch_size, model_path='rnn_model.h5'):
     rnn_data = get_rnn_data()
     x_train, y_train = rnn_data[0], rnn_data[1]
 
     rnn_model = build_rnn_model(**model_params)
-    rnn_model.fit(x=x_train, y=y_train, epochs=epochs, batch_size=batch_size, validation_split=0.2, verbose=0)
+    training_history = rnn_model.fit(x=x_train, y=y_train, epochs=epochs, batch_size=batch_size,
+                                     validation_split=0.2, verbose=0)
 
     rnn_model.save(model_path)
+
+    return training_history
 
 
 # Returns the pre-trained model from existing .h5 file if the file exists
